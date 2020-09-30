379f632c82b6614ce59e2ebc7fff550dad6a1fda
jmaricb <jmaricb@gmail.com>
2020-10-01 00:15:05 +0200

    Project created

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Josip Maric on 01/10/2020.
+//  Copyright © 2020 Josip Maric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
